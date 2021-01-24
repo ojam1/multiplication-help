103bedde2710a3e2f3a022179e666deb481ffa8b
Olly <oranson1@gmail.com>
2021-01-24 20:45:05 +0000

    add a11y

--- src/components/Number/Number.jsx
+++ src/components/Number/Number.jsx
@@ -3,9 +3,18 @@ import React from "react";
 import Wrapper from "./number.style";
 
 const Number = ({ number, highlighted, onClickFunction }) => (
-    <Wrapper onClick={() => onClickFunction(number)} highlighted={highlighted}>
-      {number}
-    </Wrapper>
-  );
+  <Wrapper
+    onClick={() => onClickFunction(number)}
+    highlighted={highlighted}
+    number={number}
+    onKeyPress={(e) => {
+      if (e.code === "Enter") {
+        onClickFunction(number);
+      }
+    }}
+  >
+    {number}
+  </Wrapper>
+);
 
 export default Number;
--- src/components/Number/number.style.js
+++ src/components/Number/number.style.js
@@ -1,15 +1,26 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-export default styled.div`
+export default styled.div.attrs(({ number }) => ({
+  tabIndex: 0,
+  "aria-label": `number ${number}`,
+}))`
   cursor: pointer;
   display: flex;
   justify-content: center;
   align-items: center;
-  font-size: 2rem;
+  font-size: 3rem;
   min-width: 20rem;
   min-height: 20rem;
   padding: 1rem;
   color: black;
   border: 2px solid black;
-  background-color: ${({ highlighted }) => highlighted ? 'yellow' : 'white'};
+  background-color: ${({ highlighted }) => (highlighted ? "yellow" : "white")};
+
+  &:focus {
+    outline: 0;
+  }
+
+  &:focus-visible {
+    outline: 2px solid blue
+  }
 `;
